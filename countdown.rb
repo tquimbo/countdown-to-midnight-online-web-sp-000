acc112792d3931e8409b582093d2c4297d9153ca
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 13:39:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,23 +2,22 @@
 number = 10
 
 def countdown(number)
-<<<<<<< HEAD
   while number > 0
 puts "#{number} SECOND(S)!"
 number -= 1
 end
 return "HAPPY NEW YEAR!"
 end
-
-=======
-  while number > 1
-puts "#{number} SECOND(S)!"
-number -= 1
-break
-return "HAPPY NEW YEAR!"
-end
-end
->>>>>>> 4d6bf55e395f4ee1b88efdf244ba8cb63e187e97
+# 
+# =======
+#   while number > 1
+# puts "#{number} SECOND(S)!"
+# number -= 1
+# break
+# return "HAPPY NEW YEAR!"
+# end
+# end
+# 
 
 def countdown_with_sleep(number)
   sleep(number)
